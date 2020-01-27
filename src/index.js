8a46b585e4613a466b364aab9db19ef8fcd40e1e
Gunel Hajiyeva <gunelhajiyeva@Gunels-MacBook-Air.local>
2020-01-27 22:06:32 +0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import $ from "jquery";
+import "materialize-css/dist/js/materialize.min.js";
+
+import "./style.scss";
+import "materialize-css/dist/css/materialize.min.css";
+
+import image from "./images/Xia.jpg";
+
+$(".image").attr("src", image);
---
+++ webpack.config.js
@@ -0,0 +1,82 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: {
+    index: "./src/index.js"
+  },
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "[name].[hash:20].js"
+  },
+  devServer: {
+    port: 4000,
+    open: "Google Chrome"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: {
+              attrs: [":data-src", "img:src", "link:href"],
+              root: path.resolve(__dirname, "src")
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg|ico)$/i,
+        use: [
+          "file-loader",
+          {
+            loader: "image-webpack-loader",
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65
+              },
+              // optipng.enabled: false will disable optipng
+              optipng: {
+                enabled: false
+              },
+              pngquant: {
+                quality: [0.65, 0.9],
+                speed: 4
+              },
+              gifsicle: {
+                interlaced: false
+              },
+              // the webp option will enable WEBP
+              webp: {
+                quality: 75
+              }
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      inject: true,
+      chunks: ["index"],
+      filename: "index.html"
+    })
+  ]
+};
